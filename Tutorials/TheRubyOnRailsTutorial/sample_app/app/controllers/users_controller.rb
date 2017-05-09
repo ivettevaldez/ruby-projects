d5d93bd6fb25625c4e8e014047854d6eeec1851b
Silvia Valdez <silvia.valdez.e@gmail.com>
2017-05-08 18:19:28 -0600

    TheRubyOnRailsTutorial: Added login and logout functionality with tests.

--- Tutorials/TheRubyOnRailsTutorial/sample_app/app/controllers/users_controller.rb
+++ Tutorials/TheRubyOnRailsTutorial/sample_app/app/controllers/users_controller.rb
@@ -12,8 +12,8 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-  		# Handle a successful save.
-      flash
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
   	else
   		render 'new'
--- Tutorials/TheRubyOnRailsTutorial/sample_app/app/models/user.rb
+++ Tutorials/TheRubyOnRailsTutorial/sample_app/app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
 	before_save { self.email = email.downcase }
 	validates :name,  presence: true, length: { maximum: 50 }
-	validates :password,  length: { minimum: 6 }
 	
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 255 },
@@ -9,4 +8,11 @@ class User < ActiveRecord::Base
 				uniqueness: { case_sensitive: false }
 
 	has_secure_password 
+	validates :password,  length: { minimum: 6 }
+
+	def User.digest(string)
+		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+													 BCrypt::Engine.cost
+		BCrypt::Password.create(string, cost: cost)
+	end
 end
