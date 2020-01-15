599aa5319545b43438e13038e3a2aac6c5e3080f
Ivette Valdez <ivette.valdez.e@gmail.com>
2020-01-15 14:42:49 +0100

    RubyOnRailsIn60Min: Part 2.

--- Tutorials/RubyOnRailsIn60Min/simpleblog/app/controllers/posts_controller.rb
+++ Tutorials/RubyOnRailsIn60Min/simpleblog/app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
     end
 
     def new
-
+        @post = Post.new
     end
 
     def create
@@ -16,7 +16,32 @@ class PostsController < ApplicationController
         @post = Post.new(post_params)
         @post.save
 
-        redirect_to @post
+        if (@post.save)
+            redirect_to @post
+        else
+            render 'new'
+        end
+    end
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:id])
+
+        if (@post.update(post_params))
+            redirect_to @post
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+
+        redirect_to posts_path
     end
 
     private def post_params
