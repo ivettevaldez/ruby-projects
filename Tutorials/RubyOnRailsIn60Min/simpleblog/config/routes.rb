7ec43e42542b0f875d4ff9ed0a5e1297dad42f8a
Ivette Valdez <ivette.valdez.e@gmail.com>
2020-01-15 15:04:58 +0100

    RubyOnRailsIn60Min: Part 3.

--- Tutorials/RubyOnRailsIn60Min/simpleblog/config/routes.rb
+++ Tutorials/RubyOnRailsIn60Min/simpleblog/config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   get 'about' => 'pages#about', as: 'about'
 
   # Example of resource route (maps HTTP verbs to controller actions).
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
 end
