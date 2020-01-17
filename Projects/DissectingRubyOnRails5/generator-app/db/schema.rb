1f2b493c6934d6a7d716e8d3849dd3519eccc2c8
Ivette Valdez <ivette.valdez.e@gmail.com>
2020-01-17 18:39:15 +0100

    DissectingRubyOnRails5: Generator layout customization and Category scaffold.

--- Projects/DissectingRubyOnRails5/generator-app/db/schema.rb
+++ Projects/DissectingRubyOnRails5/generator-app/db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_17_171521) do
+ActiveRecord::Schema.define(version: 2020_01_17_173013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
