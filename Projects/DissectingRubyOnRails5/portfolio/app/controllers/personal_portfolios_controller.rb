e6418569593f4733b351b6222669abba27e36dfc
Ivette Valdez <ivette.valdez.e@gmail.com>
2020-01-21 13:08:15 +0100

    DissectingRubyOnRails5: Integrated new and create actions for Portfolio items.

--- Projects/DissectingRubyOnRails5/portfolio/app/controllers/personal_portfolios_controller.rb
+++ Projects/DissectingRubyOnRails5/portfolio/app/controllers/personal_portfolios_controller.rb
@@ -2,4 +2,20 @@ class PersonalPortfoliosController < ApplicationController
     def index
         @portfolio_items = PersonalPortfolio.all
     end
+
+    def new
+        @portfolio_item = PersonalPortfolio.new
+    end
+
+    def create
+        @portfolio_item = PersonalPortfolio.new(params.require(:personal_portfolio).permit(:title, :subtitle, :body))
+    
+        respond_to do |format|
+            if @portfolio_item.save
+                format.html { redirect_to @portfolio_item, notice: 'Portfolio item was successfully created.' }
+            else
+                format.html { render :new }
+            end
+        end
+    end
 end
