b066c0456fca4131da2ae6a314549904096f4c16
Ivette Valdez <ivette.valdez.e@gmail.com>
2020-01-22 13:26:57 +0100

    DissectingRubyOnRails5: Configured custom routes for Portfolio.

--- Projects/DissectingRubyOnRails5/portfolio/config/routes.rb
+++ Projects/DissectingRubyOnRails5/portfolio/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  get 'portfolio/:id', to: 'personal_portfolios#show', as: 'personal_portfolio_show'
 
   resources :blogs
-  resources :personal_portfolios
+  resources :personal_portfolios, except: [:show]
 
   root to: 'pages#home'
 end
