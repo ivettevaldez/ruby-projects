a363617c6d3f48acb8d424d602838b7662fec342
Ivette Valdez <ivette.valdez.e@gmail.com>
2020-01-23 11:55:31 +0100

    DissectingRubyOnRails5: Added a root route to Home Page and also nested routes.

--- Projects/DissectingRubyOnRails5/routing-app/config/routes.rb
+++ Projects/DissectingRubyOnRails5/routing-app/config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
   get 'about', to: 'pages#about'
-  get 'advertising/soifjoisdf/lead', to: 'pages#contact', as: 'lead'
+
+  # Custom hardcoded route using alias.
+  get 'advertising/soifjoisdf/contact', to: 'pages#contact', as: 'contact'
+
+  # Nested routes.
+  namespace :admin do
+    get 'dashboard/main'
+    get 'dashboard/user'
+    get 'dashboard/blog'
+  end
 
   resources :blogs
+
+  # Root page (Home).
+  root to: 'pages#home'
 end
