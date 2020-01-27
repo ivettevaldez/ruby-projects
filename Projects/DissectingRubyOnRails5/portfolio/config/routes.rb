b72a1ba5a8995bfe2cab658b3ac1e92ed3397e16
Ivette Valdez <ivette.valdez.e@gmail.com>
2020-01-27 16:11:21 +0100

    DissectingRubyOnRails5: Implemented basic authentication.

--- Projects/DissectingRubyOnRails5/portfolio/config/routes.rb
+++ Projects/DissectingRubyOnRails5/portfolio/config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   get 'react-items', to: 'personal_portfolios#react'
   get 'portfolio/:id', to: 'personal_portfolios#show', as: 'personal_portfolio_show'
+
+  resources :personal_portfolios, except: [:show]
 
   resources :blogs do
     member do
       get :toggle_status
     end
   end
-
-  resources :personal_portfolios, except: [:show]
 
   root to: 'pages#home'
 end
