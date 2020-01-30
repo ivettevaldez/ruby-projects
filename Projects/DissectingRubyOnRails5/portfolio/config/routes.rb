ada39a1aa375c2993cb0f2a3685749ce51404a65
Ivette Valdez <ivette.valdez.e@gmail.com>
2020-01-30 17:05:34 +0100

    DissectingRubyOnRails5: Implemented Twitter call on tech news page.

--- Projects/DissectingRubyOnRails5/portfolio/config/routes.rb
+++ Projects/DissectingRubyOnRails5/portfolio/config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 
-  get 'about-me', to: 'pages#about'
-  get 'contact', to: 'pages#contact'
   get 'react-items', to: 'personal_portfolios#react'
   get 'portfolio/:id', to: 'personal_portfolios#show', as: 'personal_portfolio_show'
 
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  get 'tech-news', to: 'pages#tech_news'
+  
   resources :personal_portfolios, except: [:show]
 
   resources :blogs do
