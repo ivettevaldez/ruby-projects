33d074a339e66d079f2c99d39b5a83cb28fc1394
Ivette Valdez <ivette.valdez.e@gmail.com>
2020-01-30 17:18:34 +0100

    DissectingRubyOnRails5: WIP: Building Twitter parser for tweet links.

--- Projects/DissectingRubyOnRails5/portfolio/app/helpers/pages_helper.rb
+++ Projects/DissectingRubyOnRails5/portfolio/app/helpers/pages_helper.rb
@@ -1,2 +1,11 @@
 module PagesHelper
+    def twitter_parser tweet
+        regex = %r{
+
+        }ix
+
+        tweet.gsub(regex) do |url|
+            "<a href='#{url}' target='_blank'>#{url}</a>"
+        end.html_safe
+    end
 end
