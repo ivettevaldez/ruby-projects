b0ac6055ab7fbe7d7116f1603c06c792a9ca5166
Ivette Valdez <ivette.valdez.e@gmail.com>
2020-02-21 18:58:20 +0100

    DissectingRubyOnRails5: Implemented initial design for portfolio index page.

--- Projects/DissectingRubyOnRails5/portfolio/app/javascript/packs/application.js
+++ Projects/DissectingRubyOnRails5/portfolio/app/javascript/packs/application.js
@@ -7,10 +7,14 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('jquery')
 
-//= require jquery3
+//= require rails-ujs
+//= require jquery
 //= require popper
 //= require bootstrap-sprockets
+//= require turbolinks
+//= require_tree .
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
