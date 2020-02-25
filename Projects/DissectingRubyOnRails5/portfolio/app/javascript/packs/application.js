0741fc059367aae5e3120781697e4ac944bb5ef0
Ivette Valdez <ivette.valdez.e@gmail.com>
2020-02-25 18:45:45 +0100

    DissectingRubyOnRails5: Installed jquery ui, sortable html5 library and css calls.

--- Projects/DissectingRubyOnRails5/portfolio/app/javascript/packs/application.js
+++ Projects/DissectingRubyOnRails5/portfolio/app/javascript/packs/application.js
@@ -4,15 +4,17 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require('jquery')
+require('custom/html.sortable')
+require("turbolinks").start()
 
-//= require rails-ujs
 //= require jquery
-//= require popper
 //= require bootstrap-sprockets
+//= require jquery-ujs
+//= require jquery-ui
+//= require html.sortable
 //= require turbolinks
 //= require_tree .
 
